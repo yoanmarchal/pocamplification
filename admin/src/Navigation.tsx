0389cc0e48ab86a3ef321a3aaae0c41216a442bd
Marchal yoan <marchalyoan@gmail.com>
2021-04-17 18:13:29 +0200

    amplication-build-cknlxwdxb622511bs6fehle7ax

---
+++ admin/src/Navigation.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Panel, PanelHeader, EnumPanelStyle } from "@amplication/design-system";
+
+const Navigation = (): React.ReactElement => {
+  return (
+    <>
+      <NavigationItem name="Users" to="/users" />
+    </>
+  );
+};
+
+export default Navigation;
+
+const NavigationItem = ({
+  to,
+  name,
+}: {
+  to: string;
+  name: string;
+}): React.ReactElement => (
+  <Link to={to}>
+    <Panel panelStyle={EnumPanelStyle.Bordered}>
+      <PanelHeader>{name}</PanelHeader>
+      Create, update, search and delete {name}
+    </Panel>
+  </Link>
+);
